e1e010fa673e3bde8ac99e0c53d53b779429b3e5
nomklnog <se56160103@gmail.com>
2018-04-23 16:57:52 +0700

    fix redirect to activity if this permission can go to activity and other redirect to dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -10,16 +10,17 @@ class HomeController extends Controller
 {
     public function index(){
         $permission = \App\Models\AdminMenu::ActiveMenu()->get();
-        // if(sizeof($permission)!=0) {
+        // dd($permission);
+        if(sizeof($permission)!=0 && $permission[0]->name =="กิจกรรม") {
             //dd(\Session::get('permission'));
-        //     return redirect('admin/Activities');
-        // } else {
+            return redirect('admin/Activities');
+        } else {
             $data['main_menu'] = 'dashboard';
             $data['sub_menu'] = '';
             $data['title_page'] = 'หน้าแรก';
             $data['permission'] = Permission::CheckPermission($data['main_menu'],$data['sub_menu']);
             $data['menus'] = \App\Models\AdminMenu::ActiveMenu()->get();
             return view('Admin.dashboard',$data);
-        // }
+        }
     }
 }
